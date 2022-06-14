2475bf2a502616193592ee168bc83687141db804
Sean-Miningah <miningahsean@gmail.com>
2022-06-14 19:27:01 +0300

    NFTCard component styling

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,9 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import { COLORS, SIZES, SHADOWS, assets } from '../constants';
 import React from 'react'
 
-const NFTCard = () => {
+const NFTCard = ({ data }) => {
+  const navigation = useNavigation();
+  
   return (
-    <View>
+    <View style={{
+      backgroundColor: COLORS.white,
+      borderRadius: SIZES.font, 
+      marginBottom: SIZES.extraLarge, 
+      margin: SIZES.base, 
+      ...SHADOWS.dark
+    }}>
       <Text>NFTCard</Text>
     </View>
   )
