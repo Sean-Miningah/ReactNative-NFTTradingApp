83613743da3c94105380e3b0c1b00f05237be7a9
Sean-Miningah <miningahsean@gmail.com>
2022-06-17 18:09:29 +0300

    Details page complete

--- components/index.js
+++ components/index.js
@@ -4,9 +4,10 @@ import NFTCard from './NFTCard';
 import DetailsBid from './DetailsBid';
 import DetailsDesc from './DetailsDesc';
 import { CircleButton, RectButton } from './Button';
+import { SubInfo } from './SubInfo';
 
 
 export {
-    HomeHeader, NFTCard, FocusedStatusBar, 
+    HomeHeader, NFTCard, FocusedStatusBar, SubInfo,
     CircleButton, RectButton, DetailsBid, DetailsDesc
 }
