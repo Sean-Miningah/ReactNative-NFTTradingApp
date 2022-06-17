b6ab51444d952c269696ee2ad77aeb2b0c9a9312
Sean-Miningah <miningahsean@gmail.com>
2022-06-17 16:37:18 +0300

    Detils component files creation

---
+++ components/DetailsDesc.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const DetailsDesc = () => {
+  return (
+    <View>
+      <Text>DetailsDEsc</Text>
+    </View>
+  )
+}
+
+export default DetailsDesc
--- components/index.js
+++ components/index.js
@@ -1,10 +1,12 @@
 import HomeHeader from './HomeHeader';
 import FocusedStatusBar from './FocusedStatusBar';
 import NFTCard from './NFTCard';
+import DetailsBid from './DetailsBid';
+import DetailsDesc from './DetailsDesc';
 import { CircleButton, RectButton } from './Button';
 
 
 export {
     HomeHeader, NFTCard, FocusedStatusBar, 
-    CircleButton, RectButton
+    CircleButton, RectButton, DetailsBid, DetailsDesc
 }
